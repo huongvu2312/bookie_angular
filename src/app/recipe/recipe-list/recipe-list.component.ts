b70695cf0de5bd435dae46b630cff0a5e640d1c2
huongvu2312 <calm.and.chaotic@gmail.com>
2019-11-09 22:23:30 +0100

    initial commit

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/recipe/recipe.model';
+// import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Steak', 'Nice delicious steak',
+    'https://www.omahasteaks.com/blog/wp-content/uploads/2019/09/Grilling-Flat-Irons-BP-1080x610.jpg'),
+    new Recipe('Salat', 'Actually this is not salad',
+    'https://www.omahasteaks.com/blog/wp-content/uploads/2019/09/Grilling-Flat-Irons-BP-1080x610.jpg')
+  ];
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
