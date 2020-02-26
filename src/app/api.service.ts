f09dfa5bc79f4ddc2830277df362510ae36306f5
huongvu2312 <calm.and.chaotic@gmail.com>
2020-02-27 00:11:02 +0100

    test with json server

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ApiService {
-  private SERVER_URL = 'http://localhost:5000/';
+  private SERVER_URL = 'http://localhost:3000/';
 
   constructor(private httpClient: HttpClient) { }
 
